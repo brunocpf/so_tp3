be772b2fdabb4da0cbb6379ceea6ab4053c8e6c9
Bruno Cesar Pimenta Fernandes <brunocpf@dcc.ufmg.br>
2017-11-05 11:31:44 -0200

    Now compilable

--- dcc_os_fsck.c
+++ dcc_os_fsck.c
@@ -1,13 +1,15 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <linux/fs.h>
-#include <linux/ext2_fs.h>
+#include "ext2.h"
+
+
+
 
 #define BASE_OFFSET 1024 // location of the super-block in the first group
 #define BLOCK_OFFSET(block) (BASE_OFFSET + (block-1)*block_size)
-
-typedef int __le32;
 
 void check_fun1(int);
 void check_fun2(int);
@@ -30,7 +32,7 @@ int main(int argc, char **argv) {
         printf("Error: Input file not entered.\nUsage: dcc_os_fsck <path>");
         exit(EXIT_FAILURE);
     }
-    
+
     int sd; // input file descriptor
     
     sd = open(argv[1], O_RDWR);
@@ -49,7 +51,7 @@ int main(int argc, char **argv) {
     check_fun1(sd);
 
     group_count = 1 + (super.s_blocks_count-1) / super.s_blocks_per_group; // calculate number of block groups on the disk
-    descr_list_size = group_count * sizeof(struct ext2_group_descr); // calculate size of the group descriptor list in bytes
+    descr_list_size = group_count * sizeof(struct ext2_group_desc); // calculate size of the group descriptor list in bytes
     block_size = 1024 << super.s_log_block_size; // calculate block size in bytes
 
     /* [Fun 2]: check if magic number matches */
@@ -68,16 +70,14 @@ int main(int argc, char **argv) {
 
 /* ---------- Auxiliary Functions --------- */
 int is_attacked(struct ext2_super_block super_block) {
-    if (super_block == NULL ||
-       (super.s_magic == NULL) ||        
-       (super.s_magic == EXT2_SUPER_MAGIC))
+    if (super.s_magic != EXT2_SUPER_MAGIC)
         return 1;
     else return 0;
 }
 
-void copy_super(struct ext2_super_block super_block) {
-    lseek(sd, BASE_OFFSET, SEEK_SET);
-    write(sd, &super_block, sizeof(super_block));
+void copy_super(int fd, struct ext2_super_block super_block) {
+    lseek(fd, BASE_OFFSET, SEEK_SET);
+    write(fd, &super_block, sizeof(super_block));
 }
 /* ---------------------------------------- */
 
@@ -88,28 +88,27 @@ void check_fun1(int fd) {
     struct ext2_super_block temp_super;
 
     if (is_attacked(super)) {
-        printf("Disk error found:\n
-                [Fun 1] Super-block magic number does not match and the disk could not be read.\nThe super-block might have been attacked or the file system is not ext2.\n\nDo you wish to attempt to recover it by using a backup on the disk? (Y/<N>)");
+        printf("Disk error found:\n[Fun 1] Super-block magic number does not match and the disk could not be read.\nThe super-block might have been attacked or the file system is not ext2.\n\nDo you wish to attempt to recover it by using a backup on the disk? (Y/<N>)");
         scanf("%c", &input);
-        printf("\n")
-        if (input == "Y") {
+        printf("\n");
+        if (input == 'Y') {
             for(i = 0; i < 3; i++) {
-                printf("Checking backup at addr %d...\n", super_backup_location[i]);
-                lseek(sd, super_backup_location[i], SEEK_SET);
-                read(sd, &temp_super, sizeof(temp_super));
+                printf("Checking backup at addr %d...\n", super_backup_locations[i]);
+                lseek(fd, super_backup_locations[i], SEEK_SET);
+                read(fd, &temp_super, sizeof(temp_super));
                 if (!is_attacked(temp_super)) {
                     printf("Backup found, now copying...\n");
-                    copy_super(temp_super);
+                    copy_super(fd, temp_super);
                     printf("Backup complete.\n");
                     return;
                 } else {
                     printf("Could not be used.\n");
                 }
             }
-            printf("Backup could not be found.")
+            printf("Backup could not be found.");
         }
     }
-    printf("Could not continue fsck.\nExiting with errors.\n");    
+    printf("Could not complete fsck.\nExiting with errors.\n");    
     exit(EXIT_FAILURE);
 }
 
---
+++ ext2.h
@@ -0,0 +1,201 @@
+ #include <linux/fs.h>
+ #include <linux/types.h>
+ #include <sys/stat.h>
+ #include <linux/magic.h>
+ #include <asm/byteorder.h>
+  
+ /*
+  * Constants relative to the data blocks
+  */
+ #define	EXT2_NDIR_BLOCKS		12
+ #define	EXT2_IND_BLOCK			EXT2_NDIR_BLOCKS
+ #define	EXT2_DIND_BLOCK			(EXT2_IND_BLOCK + 1)
+ #define	EXT2_TIND_BLOCK			(EXT2_DIND_BLOCK + 1)
+ #define	EXT2_N_BLOCKS			(EXT2_TIND_BLOCK + 1) 
+
+ /*
+ * Inode flags (GETFLAGS/SETFLAGS)
+ */
+#define	EXT2_SECRM_FL			FS_SECRM_FL	/* Secure deletion */
+#define	EXT2_UNRM_FL			FS_UNRM_FL	/* Undelete */
+#define	EXT2_COMPR_FL			FS_COMPR_FL	/* Compress file */
+#define EXT2_SYNC_FL			FS_SYNC_FL	/* Synchronous updates */
+#define EXT2_IMMUTABLE_FL		FS_IMMUTABLE_FL	/* Immutable file */
+#define EXT2_APPEND_FL			FS_APPEND_FL	/* writes to file may only append */
+#define EXT2_NODUMP_FL			FS_NODUMP_FL	/* do not dump file */
+#define EXT2_NOATIME_FL			FS_NOATIME_FL	/* do not update atime */
+/* Reserved for compression usage... */
+#define EXT2_DIRTY_FL			FS_DIRTY_FL
+#define EXT2_COMPRBLK_FL		FS_COMPRBLK_FL	/* One or more compressed clusters */
+#define EXT2_NOCOMP_FL			FS_NOCOMP_FL	/* Don't compress */
+#define EXT2_ECOMPR_FL			FS_ECOMPR_FL	/* Compression error */
+/* End compression flags --- maybe not all used */	
+#define EXT2_BTREE_FL			FS_BTREE_FL	/* btree format dir */
+#define EXT2_INDEX_FL			FS_INDEX_FL	/* hash-indexed directory */
+#define EXT2_IMAGIC_FL			FS_IMAGIC_FL	/* AFS directory */
+#define EXT2_JOURNAL_DATA_FL		FS_JOURNAL_DATA_FL /* Reserved for ext3 */
+#define EXT2_NOTAIL_FL			FS_NOTAIL_FL	/* file tail should not be merged */
+#define EXT2_DIRSYNC_FL			FS_DIRSYNC_FL	/* dirsync behaviour (directories only) */
+#define EXT2_TOPDIR_FL			FS_TOPDIR_FL	/* Top of directory hierarchies*/
+#define EXT2_RESERVED_FL		FS_RESERVED_FL	/* reserved for ext2 lib */
+
+#define EXT2_FL_USER_VISIBLE		FS_FL_USER_VISIBLE	/* User visible flags */
+#define EXT2_FL_USER_MODIFIABLE		FS_FL_USER_MODIFIABLE	/* User modifiable flags */
+
+/* Flags that should be inherited by new inodes from their parent. */
+#define EXT2_FL_INHERITED (EXT2_SECRM_FL | EXT2_UNRM_FL | EXT2_COMPR_FL |\
+			   EXT2_SYNC_FL | EXT2_NODUMP_FL |\
+			   EXT2_NOATIME_FL | EXT2_COMPRBLK_FL |\
+			   EXT2_NOCOMP_FL | EXT2_JOURNAL_DATA_FL |\
+			   EXT2_NOTAIL_FL | EXT2_DIRSYNC_FL)
+
+/* Flags that are appropriate for regular files (all but dir-specific ones). */
+#define EXT2_REG_FLMASK (~(EXT2_DIRSYNC_FL | EXT2_TOPDIR_FL))
+
+/* Flags that are appropriate for non-directories/regular files. */
+#define EXT2_OTHER_FLMASK (EXT2_NODUMP_FL | EXT2_NOATIME_FL)
+
+
+ /*
+ * Structure of the super block
+ */
+struct ext2_super_block {
+	__le32	s_inodes_count;		/* Inodes count */
+	__le32	s_blocks_count;		/* Blocks count */
+	__le32	s_r_blocks_count;	/* Reserved blocks count */
+	__le32	s_free_blocks_count;	/* Free blocks count */
+	__le32	s_free_inodes_count;	/* Free inodes count */
+	__le32	s_first_data_block;	/* First Data Block */
+	__le32	s_log_block_size;	/* Block size */
+	__le32	s_log_frag_size;	/* Fragment size */
+	__le32	s_blocks_per_group;	/* # Blocks per group */
+	__le32	s_frags_per_group;	/* # Fragments per group */
+	__le32	s_inodes_per_group;	/* # Inodes per group */
+	__le32	s_mtime;		/* Mount time */
+	__le32	s_wtime;		/* Write time */
+	__le16	s_mnt_count;		/* Mount count */
+	__le16	s_max_mnt_count;	/* Maximal mount count */
+	__le16	s_magic;		/* Magic signature */
+	__le16	s_state;		/* File system state */
+	__le16	s_errors;		/* Behaviour when detecting errors */
+	__le16	s_minor_rev_level; 	/* minor revision level */
+	__le32	s_lastcheck;		/* time of last check */
+	__le32	s_checkinterval;	/* max. time between checks */
+	__le32	s_creator_os;		/* OS */
+	__le32	s_rev_level;		/* Revision level */
+	__le16	s_def_resuid;		/* Default uid for reserved blocks */
+	__le16	s_def_resgid;		/* Default gid for reserved blocks */
+	/*
+	 * These fields are for EXT2_DYNAMIC_REV superblocks only.
+	 *
+	 * Note: the difference between the compatible feature set and
+	 * the incompatible feature set is that if there is a bit set
+	 * in the incompatible feature set that the kernel doesn't
+	 * know about, it should refuse to mount the filesystem.
+	 * 
+	 * e2fsck's requirements are more strict; if it doesn't know
+	 * about a feature in either the compatible or incompatible
+	 * feature set, it must abort and not try to meddle with
+	 * things it doesn't understand...
+	 */
+	__le32	s_first_ino; 		/* First non-reserved inode */
+	__le16   s_inode_size; 		/* size of inode structure */
+	__le16	s_block_group_nr; 	/* block group # of this superblock */
+	__le32	s_feature_compat; 	/* compatible feature set */
+	__le32	s_feature_incompat; 	/* incompatible feature set */
+	__le32	s_feature_ro_compat; 	/* readonly-compatible feature set */
+	__u8	s_uuid[16];		/* 128-bit uuid for volume */
+	char	s_volume_name[16]; 	/* volume name */
+	char	s_last_mounted[64]; 	/* directory where last mounted */
+	__le32	s_algorithm_usage_bitmap; /* For compression */
+	/*
+	 * Performance hints.  Directory preallocation should only
+	 * happen if the EXT2_COMPAT_PREALLOC flag is on.
+	 */
+	__u8	s_prealloc_blocks;	/* Nr of blocks to try to preallocate*/
+	__u8	s_prealloc_dir_blocks;	/* Nr to preallocate for dirs */
+	__u16	s_padding1;
+	/*
+	 * Journaling support valid if EXT3_FEATURE_COMPAT_HAS_JOURNAL set.
+	 */
+	__u8	s_journal_uuid[16];	/* uuid of journal superblock */
+	__u32	s_journal_inum;		/* inode number of journal file */
+	__u32	s_journal_dev;		/* device number of journal file */
+	__u32	s_last_orphan;		/* start of list of inodes to delete */
+	__u32	s_hash_seed[4];		/* HTREE hash seed */
+	__u8	s_def_hash_version;	/* Default hash version to use */
+	__u8	s_reserved_char_pad;
+	__u16	s_reserved_word_pad;
+	__le32	s_default_mount_opts;
+ 	__le32	s_first_meta_bg; 	/* First metablock block group */
+	__u32	s_reserved[190];	/* Padding to the end of the block */
+};
+
+struct ext2_group_desc
+{
+	__le32	bg_block_bitmap;		/* Blocks bitmap block */
+	__le32	bg_inode_bitmap;		/* Inodes bitmap block */
+	__le32	bg_inode_table;		/* Inodes table block */
+	__le16	bg_free_blocks_count;	/* Free blocks count */
+	__le16	bg_free_inodes_count;	/* Free inodes count */
+	__le16	bg_used_dirs_count;	/* Directories count */
+	__le16	bg_pad;
+	__le32	bg_reserved[3];
+};
+
+/*
+ * Structure of an inode on the disk
+ */
+ struct ext2_inode {
+	__le16	i_mode;		/* File mode */
+	__le16	i_uid;		/* Low 16 bits of Owner Uid */
+	__le32	i_size;		/* Size in bytes */
+	__le32	i_atime;	/* Access time */
+	__le32	i_ctime;	/* Creation time */
+	__le32	i_mtime;	/* Modification time */
+	__le32	i_dtime;	/* Deletion Time */
+	__le16	i_gid;		/* Low 16 bits of Group Id */
+	__le16	i_links_count;	/* Links count */
+	__le32	i_blocks;	/* Blocks count */
+	__le32	i_flags;	/* File flags */
+	union {
+		struct {
+			__le32  l_i_reserved1;
+		} linux1;
+		struct {
+			__le32  h_i_translator;
+		} hurd1;
+		struct {
+			__le32  m_i_reserved1;
+		} masix1;
+	} osd1;				/* OS dependent 1 */
+	__le32	i_block[EXT2_N_BLOCKS];/* Pointers to blocks */
+	__le32	i_generation;	/* File version (for NFS) */
+	__le32	i_file_acl;	/* File ACL */
+	__le32	i_dir_acl;	/* Directory ACL */
+	__le32	i_faddr;	/* Fragment address */
+	union {
+		struct {
+			__u8	l_i_frag;	/* Fragment number */
+			__u8	l_i_fsize;	/* Fragment size */
+			__u16	i_pad1;
+			__le16	l_i_uid_high;	/* these 2 fields    */
+			__le16	l_i_gid_high;	/* were reserved2[0] */
+			__u32	l_i_reserved2;
+		} linux2;
+		struct {
+			__u8	h_i_frag;	/* Fragment number */
+			__u8	h_i_fsize;	/* Fragment size */
+			__le16	h_i_mode_high;
+			__le16	h_i_uid_high;
+			__le16	h_i_gid_high;
+			__le32	h_i_author;
+		} hurd2;
+		struct {
+			__u8	m_i_frag;	/* Fragment number */
+			__u8	m_i_fsize;	/* Fragment size */
+			__u16	m_pad1;
+			__u32	m_i_reserved2[2];
+		} masix2;
+	} osd2;				/* OS dependent 2 */
+};
