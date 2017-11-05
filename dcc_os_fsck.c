98d1b2c47fd4d6980becd36a5b2b05c5177a1796
Bruno Cesar Pimenta Fernandes <brunocpf@dcc.ufmg.br>
2017-11-05 18:09:19 -0200

    Fun 2 implemented

--- dcc_os_fsck.c
+++ dcc_os_fsck.c
@@ -7,6 +7,10 @@
 
 #define BASE_OFFSET 1024 // location of the super-block in the first group
 #define BLOCK_OFFSET(block) (BASE_OFFSET + (block-1)*block_size)
+#define CHAR_SIZE ((int)(sizeof(char) * 8))
+#define TEST_BIT(ary, bitno) ( (ary[(bitno)/CHAR_SIZE] & (1 << ((bitno)%CHAR_SIZE) )) != 0 )
+#define CLEAR_BIT(ary, bitno) ( ary[((bitno)/CHAR_SIZE)] &= ~(1 << ((bitno)%CHAR_SIZE)) )            
+
 
 void check_fun1(int);
 void check_fun2(int);
@@ -21,12 +25,13 @@ struct ext2_super_block super;
 unsigned int group_count;
 unsigned int descr_list_size;
 unsigned int block_size;
+unsigned int group_size;
 /* ----------------- */
 
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("Error: Input file not entered.\nUsage: dcc_os_fsck <path>");
+        printf("Error: Input file not entered.\nUsage: dcc_os_fsck <path>\n");
         exit(EXIT_FAILURE);
     }
 
@@ -35,7 +40,7 @@ int main(int argc, char **argv) {
     sd = open(argv[1], O_RDWR);
 
     if (sd < 0) {
-        printf("Error: Unable to open input file.\nUsage: dcc_os_fsck <path>");
+        printf("Error: Unable to open input file.\nUsage: dcc_os_fsck <path>\n");
         exit(EXIT_FAILURE);
     }
 
@@ -44,25 +49,22 @@ int main(int argc, char **argv) {
     lseek(sd, BASE_OFFSET, SEEK_SET); // position head above first super-block
     read(sd, &super, sizeof(super)); // read superblock
 
-    printf("Sizeof: %d %d\nMagic: %d\n", (int) sizeof(super), (int) sizeof(struct ext2_super_block), super.s_magic);
-
-
-    /* [Fun 1]: check if super-block was attacked */
+    printf("\n--> [Fun 1]: Checking super-block...\n");
     check_fun1(sd);
-
+    
     group_count = 1 + (super.s_blocks_count-1) / super.s_blocks_per_group; // calculate number of block groups on the disk
     descr_list_size = group_count * sizeof(struct ext2_group_desc); // calculate size of the group descriptor list in bytes
     block_size = 1024 << super.s_log_block_size; // calculate block size in bytes
-
-    /* [Fun 2]: check if magic number matches */
+    group_size = block_size * super.s_blocks_per_group;
+    
+    printf("\n--> [Fun 2]: Checking for multiply-owned blocks...\n");    
     check_fun2(sd);
 
     // Fun 3
-    check_fun3(sd);
+    //check_fun3(sd);
 
     // Fun 4
     check_fun4(sd);
-
 
     printf("fsck exiting.\nNo errors found.\n");
     exit(EXIT_SUCCESS);
@@ -70,9 +72,11 @@ int main(int argc, char **argv) {
 
 /* ---------- Auxiliary Functions --------- */
 int is_attacked(struct ext2_super_block super_block) {
-    if (super_block.s_magic != EXT2_SUPER_MAGIC)
-        return 1;
-    else return 0;
+    return (super_block.s_magic != EXT2_SUPER_MAGIC);
+}
+
+int is_permission_corrupted(struct ext2_inode inode) {
+    return ((inode.i_mode & 0xff) == 0);
 }
 
 void copy_super(int fd, struct ext2_super_block super_block) {
@@ -82,8 +86,13 @@ void copy_super(int fd, struct ext2_super_block super_block) {
     lseek(fd, BASE_OFFSET, SEEK_SET);
     read(fd, &super, sizeof(super));
 }
-/* ---------------------------------------- */
 
+static void read_inode(int fd, int inode_no, const struct ext2_group_desc *group, unsigned int group_no, struct ext2_inode *inode) {
+    unsigned int offset = BLOCK_OFFSET(group->bg_inode_table) + group_size * group_no;
+    lseek(fd, offset+(inode_no-1)*sizeof(struct ext2_inode), SEEK_SET);
+    read(fd, inode, sizeof(struct ext2_inode));
+}
+/* ---------------------------------------- */
 
 void check_fun1(int fd) {
     short i;
@@ -91,8 +100,8 @@ void check_fun1(int fd) {
     struct ext2_super_block temp_super;
 
     if (is_attacked(super)) {
-        printf("Disk error found:\n[Fun 1] Super-block magic number does not match and the disk could not be read.\nThe super-block might have been attacked or the file system is not ext2.\n\nDo you wish to attempt to recover it by using a backup on the disk? (Y/<N>)");
-        scanf("%c", &input);
+        printf("Disk error found:\nSuper-block magic number does not match and the disk could not be read.\nThe super-block might have been attacked or the file system is not ext2.\n\nDo you wish to attempt to recover it by using a backup on the disk? (Y/<N>)\n");
+        scanf(" %c", &input);
         printf("\n");
         if (input == 'Y' || input == 'y') {
             for(i = 0; i < 3; i++) {
@@ -120,11 +129,157 @@ void check_fun1(int fd) {
 }
 
 void check_fun2(int fd) {
-    
+    int i, j, k, l;
+    char input;
+    unsigned int offset;
+    struct ext2_group_desc temp_group_desc;
+    struct ext2_inode temp_inode;
+    unsigned char owned_blocks[super.s_blocks_count];
+
+    for(i = 0; i < super.s_blocks_count; i++)
+        owned_blocks[i] = 0;
+
+    // Checking owned blocks
+    for(i = 0; i < group_count; i++) {
+        offset = BASE_OFFSET + group_size * i;
+        printf("Checking for inodes in group %d, offset %d...\n", i, offset);
+        lseek(fd, offset + block_size, SEEK_SET);  /* position head above the group descriptor block */
+        read(fd, &temp_group_desc, sizeof(temp_group_desc));
+
+        unsigned char *d_bmap = malloc(block_size);;
+        unsigned char *i_bmap = malloc(block_size);
+
+        lseek(fd, BLOCK_OFFSET(temp_group_desc.bg_block_bitmap), SEEK_SET);
+        read(fd, d_bmap, block_size);
+
+        lseek(fd, BLOCK_OFFSET(temp_group_desc.bg_inode_bitmap), SEEK_SET);
+        read(fd, i_bmap, block_size);
+        
+        for(j = 1; j <= super.s_inodes_per_group; j++) {
+            read_inode(fd, j, &temp_group_desc, i, &temp_inode);
+            if(!TEST_BIT(i_bmap, j-1) || temp_inode.i_size <= 0) // check if inode is valid
+                continue;
+            owned_blocks[temp_inode.i_block[0]]++;
+        }
+        free(d_bmap);
+        free(i_bmap);
+    }
+
+
+    for(l = 0; l < super.s_blocks_count; l++) {
+        if (owned_blocks[l] > 1) {
+            printf("Multiply-owned block found: %d\n", l);
+            printf("Do you wish to delete one of its owners? (Y/<N>)\n");
+            scanf(" %c", &input);
+            printf("\n");
+            if (input == 'Y' || input == 'y') {
+                for(i = 0; i < group_count; i++) {
+                    offset = BASE_OFFSET + group_size * i;
+                    lseek(fd, offset + block_size, SEEK_SET);  /* position head above the group descriptor block */
+                    read(fd, &temp_group_desc, sizeof(temp_group_desc));
+            
+                    unsigned char *d_bmap = malloc(block_size);;
+                    unsigned char *i_bmap = malloc(block_size);
+            
+                    lseek(fd, BLOCK_OFFSET(temp_group_desc.bg_block_bitmap) + group_size * i, SEEK_SET);
+                    read(fd, d_bmap, block_size);
+            
+                    lseek(fd, BLOCK_OFFSET(temp_group_desc.bg_inode_bitmap) + group_size * i, SEEK_SET);
+                    read(fd, i_bmap, block_size);
+                    
+                    int found = 0;
+                    for(j = 1; j <= super.s_inodes_per_group; j++) {
+                        read_inode(fd, j, &temp_group_desc, i, &temp_inode);
+                        if(!TEST_BIT(i_bmap, j-1) || temp_inode.i_size <= 0) // check if inode is valid
+                            continue;
+                        if (temp_inode.i_block[0] == l) {
+                            found = 1;
+                            break;
+                        }
+                    }
+                    if(found) {
+                        printf("Inode deleted.\n");
+                        CLEAR_BIT(i_bmap, j-1);
+                        lseek(fd, BLOCK_OFFSET(temp_group_desc.bg_inode_bitmap) + group_size * i, SEEK_SET);
+                        write(fd, i_bmap, block_size);
+                        owned_blocks[l]--;
+                        free(d_bmap);
+                        free(i_bmap);
+                        break;
+                    }
+                    free(d_bmap);
+                    free(i_bmap);
+                }
+            } else {
+                printf("Could not complete fsck.\nExiting with errors.\n");    
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+
+    for(i = 0; i < super.s_blocks_count; i++) {
+        if(owned_blocks[i] > 1) {
+            printf("Multiply-owned blocks still exist, checking again...\n");
+            check_fun2(fd);
+        }
+    }
+
+    printf("[Fun 2] OK: No multiply-owned blocks.\n");
+
 }
 
 void check_fun3(int fd) {
-    
+    short i, j, k;
+    char input;
+    unsigned int offset;
+    struct ext2_group_desc temp_group_desc;
+    struct ext2_inode temp_inode;
+
+    for(i = 0; i < group_count; i++) {
+        offset = block_size * super.s_blocks_per_group * i;
+        lseek(fd, BASE_OFFSET + offset + block_size, SEEK_SET);  /* position head above the group descriptor block */
+        read(fd, &temp_group_desc, sizeof(temp_group_desc));
+
+        unsigned char *d_bmap;
+        unsigned char *i_bmap;
+
+        d_bmap = malloc(block_size);
+        i_bmap = malloc(block_size);
+
+        lseek(fd, BLOCK_OFFSET(temp_group_desc.bg_block_bitmap), SEEK_SET);
+        read(fd, d_bmap, block_size);
+
+        lseek(fd, BLOCK_OFFSET(temp_group_desc.bg_inode_bitmap), SEEK_SET);
+        read(fd, i_bmap, block_size);
+        
+        for(j = 0; j < super.s_inodes_per_group; j++) {
+            if(!TEST_BIT(i_bmap, j)) // check if inode is valid
+                continue;
+            printf("Reading inode %d/%d from group %d...\n",
+                    j, super.s_inodes_per_group, i);
+            read_inode(fd, j+1, &temp_group_desc, i, &temp_inode);
+            printf("inode file mode %d, inode size %d, inode num %d\n", temp_inode.i_mode, temp_inode.i_size, j+1);            
+            
+            
+            
+            if (is_permission_corrupted(temp_inode)) {
+                printf("Disk error found:\n[Fun 3] File permissions corrupted.\nSet permissions? (Y/<N>)");
+                scanf("%c", &input);
+                printf("\n");
+                if (input == 'Y' || input == 'y') {
+                    continue;
+                } 
+                else {
+                  printf("Could not complete fsck.\nExiting with errors.\n");    
+                  exit(EXIT_FAILURE);
+                }
+            }
+        }
+        free(d_bmap);
+        free(i_bmap);
+    }
+    printf("[Fun 3] OK: Consistent permissions.\n");
 }
 
 void check_fun4(int fd) {
